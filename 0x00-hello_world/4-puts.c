4ac6f06ccaa88c1d117b3c5e5943aadc391c6468
Okeke Victor <onyxvictor@yahoo.com>
2023-07-13 15:48:56 -0700

    PUTS

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - put function program
- * Return: 0
- */
+*main - main entry point
+*return : always 0 (success)
+*/
 int main(void)
 {
-puts{"\"Programming is like building a multilingual puzzle"};
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
