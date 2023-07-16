c58a51d4fa291dfd2fda931ae337c5dff9b17160
Okeke Victor <onyxvictor@yahoo.com>
2023-07-15 17:24:04 -0700

    Suffer

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Suffer
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
