06aa4dfd067d5b9e65ea720a464607de3179a0de
Okeke Victor <onyxvictor@yahoo.com>
2023-07-18 20:07:57 -0700

    x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,19 +2,20 @@
 /**
  * print_alphabet_x10 - Prints the alphabet 10 times
  *
- * Return: Always 0 (Success)
+ * Return: void
  */
 void print_alphabet_x10(void)
 {
-	int i;
-	int j;
-
-	for (i = 1; i <= 10; i++)
-	{
-		for (j = 97; j <= 122; j++)
-		{
-			_putchar(j);
-		}
-		_putchar('\n');
-	}
+char ch;
+int i;
+i = 0;
+while (i < 10)
+{
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+i++;
+}
 }
