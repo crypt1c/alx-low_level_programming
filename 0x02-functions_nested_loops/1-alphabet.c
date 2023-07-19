114addae39eba11fe1e45b4508747b21fc1ec27e
Okeke Victor <onyxvictor@yahoo.com>
2023-07-18 19:55:51 -0700

    Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,14 @@
 #include "main.h"
 /**
  * print_alphabet - print alphabet in lowercase
- * 
- * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
-	int i;
+	char letter;
 
-	for (i = 97; i <= 122; i++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-		_putchar(i);
+		_putchar(letter);
 	}
 	_putchar('\n');
 }
